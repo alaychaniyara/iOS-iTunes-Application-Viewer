a0fe53734c9d70048dd587818cb42bab1c3338d7
Alay Chaniyara <alaychaniyara@gmail.com>
2018-10-31 16:48:18 -0400

    Initial Commit

---
+++ iOS-InClass03Tests/iOS_InClass03Tests.swift
@@ -0,0 +1,34 @@
+//
+//  iOS_InClass03Tests.swift
+//  iOS-InClass03Tests
+//
+//  Created by Alay Chaniyara on 10/31/18.
+//  Copyright © 2018 Alay Chaniyara. All rights reserved.
+//
+
+import XCTest
+@testable import iOS_InClass03
+
+class iOS_InClass03Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
